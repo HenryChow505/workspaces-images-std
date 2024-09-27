0db8e0f8c8dfb69af2af90ee25fdc7ceee90411a
HenryChow505 <134348195+HenryChow505@users.noreply.github.com>
2024-09-27 15:14:14 +0000

    ✨ feat: 执行脚本输出为流式数据，实时地

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -6,13 +6,13 @@ BASE=$2
 DOCKERFILE=$3
 
 # Determine if we are using private images
-if [ ${USE_PRIVATE_IMAGES} -eq 1 ]; then
-  BASE=${BASE}-private
-fi
+# if [ ${USE_PRIVATE_IMAGES} -eq 1 ]; then
+#   BASE=${BASE}-private
+# fi
 
 ## Build/Push image to cache endpoint by pipeline ID ##
+  # --build-arg BASE_TAG="${BASE_TAG}" \
 docker build \
   -t KasmCustom/${NAME}:dev \
   --build-arg BASE_IMAGE="${BASE}" \
-  --build-arg BASE_TAG="${BASE_TAG}" \
   -f ${DOCKERFILE} .
